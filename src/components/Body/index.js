86228dce4600e8429cb907546233da216f60c272
vaishnavi-dev955 <vaishnavimandala1998@gmail.com>
2023-03-11 10:21:12 +0000

    second commit

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -11,18 +11,10 @@ const Body = () => (
             <div className="left-Navbar-container">
               <h1 className="left-Navbar-heading">Left Navbar Menu</h1>
               <ul className="left-navbar-style">
-                <li>
-                  <p className="left-navbar-para">Item 1</p>
-                </li>
-                <li>
-                  <p className="left-navbar-para">Item 2</p>
-                </li>
-                <li>
-                  <p className="left-navbar-para">Item 3</p>
-                </li>
-                <li>
-                  <p className="left-navbar-para">Item 4</p>
-                </li>
+                <li className="left-navbar-para">Item 1</li>
+                <li className="left-navbar-para">Item 2</li>
+                <li className="left-navbar-para">Item 3</li>
+                <li className="left-navbar-para">Item 4</li>
               </ul>
             </div>
           ) : (
@@ -30,7 +22,7 @@ const Body = () => (
           )}
           {showContent ? (
             <div className="Content-container">
-              <h1 className="Content-heading">Context</h1>
+              <h1 className="Content-heading">Content</h1>
               <p className="Content-para">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 elusmond tempor Incididunt ut labore et dolore magna aliqua. Ut
@@ -43,18 +35,14 @@ const Body = () => (
           {showRightNavbar ? (
             <div className="Right-Navbar-container">
               <h1 className="Right-Navbar-Heading">Right Navbar</h1>
-              <ul className="Right-Navbar-List">
-                <li>
-                  <div className="Right-Navbar-Box">
-                    <p className="Right-Navbar-para">Ad 1</p>
-                  </div>
-                </li>
-                <li>
-                  <div className="Right-Navbar-Box">
-                    <p className="Right-Navbar-para">Ad 2</p>
-                  </div>
-                </li>
-              </ul>
+              <div className="Right-Navbar-List">
+                <div className="Right-Navbar-Box">
+                  <p className="Right-Navbar-para">Ad 1</p>
+                </div>
+                <div className="Right-Navbar-Box">
+                  <p className="Right-Navbar-para">Ad 2</p>
+                </div>
+              </div>
             </div>
           ) : (
             ''
